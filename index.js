c5154ece91079f971f0d40d19aa08791af1a5e64
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-11 09:07:44 -0800

    commit tree d602c830b52250da467ebf91aab530d43c129acc

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random());
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(4, 5, 20);
